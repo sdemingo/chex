ce2e6b58d781c8fb54d4fc284118850521953cdb
Sergio de Mingo <sdemingo@gmail.com>
2015-10-30 15:44:22 +0100

    add timestamp, checksum and tags over questions

--- appengine/questions/question.go
+++ appengine/questions/question.go
@@ -1,8 +1,27 @@
 package questions
 
 import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"time"
+
 	"app/users"
+	"appengine/datastore"
+	"appengine/srv"
 )
+
+const (
+	ERR_NOTVALIDQUEST   = "Pregunta no valido"
+	ERR_DUPLICATEDQUEST = "Pregunta duplicada"
+	ERR_QUESTNOTFOUND   = "Pregunta no encontrada"
+)
+
+type QuestionTag struct {
+	Id      int64 `json:",string" datastore:"-"`
+	QuestId int64
+	Tag     string
+}
 
 type Question struct {
 	Id         int64        `json:",string" datastore:"-"`
@@ -10,6 +29,8 @@ type Question struct {
 	Author     *users.NUser `datastore:"-"`
 	SolutionId int64        `json:",string"`
 	Solution   *Answer      `datastore:"-"`
+	TimeStamp  time.Time    `json:"`
+	CheckSum   string       `json:"`
 
 	AType   AnswerBodyType
 	Text    string
@@ -44,4 +65,93 @@ func (q Question) SetAuthor(author *users.NUser) {
 		q.Author = author
 		q.AuthorId = author.Id
 	}
+}
+
+func (q Question) SetChecksum() {
+	s := q.Text
+	for _, op := range q.Options {
+		s = s + op
+	}
+	q.CheckSum = fmt.Sprintf("%x", md5.Sum([]byte(s)))
+}
+
+func (q Question) IsValid() error {
+	if q.Text == "" {
+		return errors.New(ERR_NOTVALIDQUEST)
+	}
+	return nil
+}
+
+func putQuestion(wr srv.WrapperRequest, q Question) error {
+	if err := q.IsValid(); err != nil {
+		return err
+	}
+
+	q.TimeStamp = time.Now()
+	q.SetChecksum()
+
+	_, err := getQuestionByChecksum(wr, q.CheckSum)
+	if err == nil {
+		return errors.New(ERR_DUPLICATEDQUEST)
+	}
+
+	key := datastore.NewKey(wr.C, "questions", "", 0, nil)
+	key, err = datastore.Put(wr.C, key, &q)
+	if err != nil {
+		return err
+	}
+	q.Id = key.IntID()
+
+	// Add a QuestionsTags entry for each tag for this questions
+	addQuestTags(wr, q)
+
+	return nil
+}
+
+func getQuestionByChecksum(wr srv.WrapperRequest, sum string) (Question, error) {
+	var qs []Question
+	var q Question
+
+	qry := datastore.NewQuery("questions").Filter("Checksum =", sum)
+
+	keys, err := qry.GetAll(wr.C, &qs)
+	if (len(keys) == 0) || err != nil {
+		return q, errors.New(ERR_QUESTNOTFOUND)
+	}
+	q = qs[0]
+	q.Id = keys[0].IntID()
+	q.Tags, _ = getQuestTags(wr, q)
+
+	return q, nil
+}
+
+func addQuestTags(wr srv.WrapperRequest, q Question) error {
+	for _, tag := range q.Tags {
+		key := datastore.NewKey(wr.C, "questions-tags", "", 0, nil)
+		qt := QuestionTag{QuestId: q.Id, Tag: tag}
+		key, err := datastore.Put(wr.C, key, &qt)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getQuestTags(wr srv.WrapperRequest, q Question) ([]string, error) {
+
+	var tags []string
+	var questionTags []QuestionTag
+
+	qry := datastore.NewQuery("questions-tags").Filter("QuestId =", q.Id)
+	_, err := qry.GetAll(wr.C, &questionTags)
+	if err != nil {
+		return tags, err
+	}
+	tags = make([]string, 0)
+	for _, qtag := range questionTags {
+		tags = append(tags, qtag.Tag)
+	}
+
+	return tags, nil
+
 }
