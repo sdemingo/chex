a0f5a5a19904aef5f94cc11893ff215e91a5fa2e
Sergio de Mingo <sdemingo@gmail.com>
2015-11-18 16:27:10 +0100

    change info modal dialog title color

--- static/js/chex-utils.js
+++ static/js/chex-utils.js
@@ -98,8 +98,8 @@ function resume(text,max){
 function showInfoMessage(text) {
     var modalData={
 	id:"dialog",
-	type:"success",
-	titleText:"Enhorabuena",
+	type:"info",
+	titleText:"Información",
 	bodyText:text
     }
 
@@ -120,104 +120,104 @@ function showErrorMessage(text) {
 }
 
 
-    $.fn.serializeObject = function()
-    {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function() {
-            if (o[this.name] !== undefined) {
-		if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-		}
-		o[this.name].push(this.value || '');
-            } else {
-		o[this.name] = this.value || '';
-            }
-	});
-	return o;
-    };
-
-    Array.prototype.clean = function(deleteValue) {
-	for (var i = 0; i < this.length; i++) {
-	    if (this[i] == deleteValue) {         
-		this.splice(i, 1);
-		i--;
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+	    if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
 	    }
-	}
-	return this;
-    };
+	    o[this.name].push(this.value || '');
+        } else {
+	    o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
 
-
-
-
-
-
-
-
-    /*
-      - Run the modal with a button, using the same id for the modal an for data-target attr form button:
-      <button id="..." type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myDialog">
-
-      - Operate the modal manually:
-      $('#myDialog').modal('toggle');
-      $('#myDialog').modal('show');
-      $('#myDialog').modal('hide');
-
-      - Init the modal with an object as:
-      var modalData={
-      id:"myDialog",
-      titleText:"Titulo del dialogo",
-      bodyText:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-      actionButton:{
-      text:"Ok",
-      handler: actionButtonAnyHandler
-      } 
-    */
-
-
-    var modal = {
-	init:function(data){
-
-	    if ($(".modal").length){
-		$(".modal").remove()
-	    }
-
-	    $("body").append(
-		$('<div class="modal fade" id="'+data.id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">')
-		    .append(
-			$('<div class="modal-dialog" role="document">')
-			    .append(
-				$('<div class="modal-content">')
-				    .append(
-					$('<div class="modal-header modal-header-'+data.type+'">')
-					    .append(
-					$('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
-					    )
-					    .append(
-						$('<h4 class="modal-title" id="myModalLabel">'+data.titleText+'</h4>')
-					    )
-				    )
-				
-				    .append(
-					$('<div class="modal-body">')
-					    .append(data.bodyText)
-				    )
-
-				    .append(
-					$('<div class="modal-footer">')
-					    .append(
-						$('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>')
-					    )
-				    )
-			    )
-		    )
-	    )
-
-	    // insert action button
-	    if (data.actionButton){
-		$(".modal .modal-footer").append(
-		    $('<button type="button" class="btn btn-primary">'+data.actionButton.text+'</button>')
-		)
-	    }
+Array.prototype.clean = function(deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+	if (this[i] == deleteValue) {         
+	    this.splice(i, 1);
+	    i--;
 	}
     }
+    return this;
+};
+
+
+
+
+
+
+
+
+/*
+  - Run the modal with a button, using the same id for the modal an for data-target attr form button:
+  <button id="..." type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myDialog">
+
+  - Operate the modal manually:
+  $('#myDialog').modal('toggle');
+  $('#myDialog').modal('show');
+  $('#myDialog').modal('hide');
+
+  - Init the modal with an object as:
+  var modalData={
+  id:"myDialog",
+  titleText:"Titulo del dialogo",
+  bodyText:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+  actionButton:{
+  text:"Ok",
+  handler: actionButtonAnyHandler
+  } 
+*/
+
+
+var modal = {
+    init:function(data){
+
+	if ($(".modal").length){
+	    $(".modal").remove()
+	}
+
+	$("body").append(
+	    $('<div class="modal fade" id="'+data.id+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">')
+		.append(
+		    $('<div class="modal-dialog" role="document">')
+			.append(
+			    $('<div class="modal-content">')
+				.append(
+				    $('<div class="modal-header modal-header-'+data.type+'">')
+					.append(
+					    $('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
+					)
+					.append(
+					    $('<h4 class="modal-title" id="myModalLabel">'+data.titleText+'</h4>')
+					)
+				)
+			    
+				.append(
+				    $('<div class="modal-body">')
+					.append(data.bodyText)
+				)
+
+				.append(
+				    $('<div class="modal-footer">')
+					.append(
+					    $('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>')
+					)
+				)
+			)
+		)
+	)
+
+	// insert action button
+	if (data.actionButton){
+	    $(".modal .modal-footer").append(
+		$('<button type="button" class="btn btn-primary">'+data.actionButton.text+'</button>')
+	    )
+	}
+    }
+}
