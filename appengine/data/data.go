c038a211cbee6796c875b0133c7d58f47beaf6bb
Sergio de Mingo <sdemingo@gmail.com>
2015-12-01 19:09:03 +0100

    working on datastore wrapper

---
+++ appengine/data/data.go
@@ -0,0 +1,73 @@
+package data
+
+import (
+	"reflect"
+
+	"appengine"
+	"appengine/datastore"
+)
+
+/*
+var registry = map[string]reflect.Type{
+	"TipoA": reflect.TypeOf(TipoA{}),
+}
+*/
+
+var registry map[string]reflect.Type
+
+func RegistryModel(name string, prot interface{}) {
+
+}
+
+type DataItem interface {
+	ID() int64
+	SetID(id int64)
+	Kind() string
+}
+
+func Put(c appengine.Context, obj DataItem) (int64, error) {
+
+	var key *datastore.Key
+	dsKind := obj.Kind()
+	if id := obj.ID(); id != 0 {
+		key = datastore.NewKey(c, dsKind, "", obj.ID(), nil)
+	} else {
+		key = datastore.NewIncompleteKey(c, dsKind, nil)
+	}
+
+	key, err := datastore.Put(c, key, obj)
+	obj.SetID(key.IntID())
+
+	return key.IntID(), err
+}
+
+type DataConn struct {
+	Entity string
+	Query  *datastore.Query
+	Ids    []int64
+	Items  []DataItem
+}
+
+func (op *DataConn) AddFilter(filter string, value interface{}) {
+	op.Query = op.Query.Filter(filter, value)
+}
+
+func (op *DataConn) GetMany(c appengine.Context, items interface{}) error {
+
+	keys, err := op.Query.GetAll(c, items)
+	if err != nil {
+		return err
+	}
+
+	for i := range keys {
+		op.Ids = append(op.Ids, keys[i].IntID())
+	}
+
+	return err
+}
+
+func (op *DataConn) Get(c appengine.Context, id int64) (DataItem, error) {
+
+	key = datastore.NewKey(c, dsKind, "", id, nil)
+	return err
+}
