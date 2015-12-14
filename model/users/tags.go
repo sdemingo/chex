f5f3c9f59dc1070d44c9bd2bb1e16a43b5df8fbd
Sergio de Mingo <sdemingo@gmail.com>
2015-12-14 19:07:32 +0100

    split user.go and tags.go

---
+++ model/users/tags.go
@@ -0,0 +1,151 @@
+package users
+
+import (
+	"errors"
+	"fmt"
+
+	"app/users"
+
+	"appengine/data"
+	"appengine/srv"
+)
+
+type UserTag struct {
+	Id     int64 `json:",string" datastore:"-"`
+	UserId int64
+	Tag    string
+}
+
+func (ut UserTag) ID() int64 {
+	return ut.Id
+}
+
+func (ut *UserTag) SetID(id int64) {
+	ut.Id = id
+}
+
+type UserTagBuffer []*UserTag
+
+func NewUserTagBuffer() UserTagBuffer {
+	return make([]*UserTag, 0)
+}
+
+func (v UserTagBuffer) At(i int) data.DataItem {
+	return data.DataItem(v[i])
+}
+
+func (v UserTagBuffer) Set(i int, t data.DataItem) {
+	v[i] = t.(*UserTag)
+}
+
+func (v UserTagBuffer) Len() int {
+	return len(v)
+}
+
+func getUsersByTags(wr srv.WrapperRequest, tags []string) ([]*users.NUser, error) {
+	nus := NewNUserBuffer()
+	uTagsAll := NewUserTagBuffer()
+
+	q := data.NewConn(wr, "users-tags")
+	if q.GetMany(&uTagsAll) != nil {
+		return nus, errors.New(users.ERR_USERNOTFOUND)
+	}
+
+	// After recover all UserTags it makes a homemade filtering
+	// because dinamically filteres based on tags array are not
+	// allowed in GAE datastore
+
+	filtered := make(map[int64]int)
+	for _, tag := range tags {
+		for _, ut := range uTagsAll {
+			if ut.Tag == tag {
+				if _, ok := filtered[ut.UserId]; !ok {
+					filtered[ut.UserId] = 1
+				} else {
+					filtered[ut.UserId]++
+				}
+			}
+		}
+	}
+
+	for id, _ := range filtered {
+		if filtered[id] == len(tags) {
+			nu, err := getUserById(wr, fmt.Sprintf("%d", id))
+			if err != nil {
+				return nus, err
+			}
+			nus = append(nus, nu)
+		}
+	}
+
+	return nus, nil
+}
+
+func getUserTags(wr srv.WrapperRequest, nu *users.NUser) ([]string, error) {
+	tags := make([]string, 0)
+	userTags := NewUserTagBuffer()
+
+	q := data.NewConn(wr, "users-tags")
+	q.AddFilter("UserId =", nu.Id)
+	if q.GetMany(&userTags) != nil {
+		return tags, errors.New(users.ERR_USERNOTFOUND)
+	}
+
+	tags = make([]string, 0)
+	for _, utag := range userTags {
+		tags = append(tags, utag.Tag)
+	}
+
+	return tags, nil
+
+}
+
+func addUserTags(wr srv.WrapperRequest, nu *users.NUser) error {
+	q := data.NewConn(wr, "users-tags")
+	for _, tag := range nu.Tags {
+		ut := &UserTag{UserId: nu.Id, Tag: tag}
+		err := q.Put(ut)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteUserTags(wr srv.WrapperRequest, nu *users.NUser) error {
+	userTags := NewUserTagBuffer()
+
+	q := data.NewConn(wr, "users-tags")
+	q.AddFilter("UserId =", nu.Id)
+	q.GetMany(&userTags)
+
+	for _, utag := range userTags {
+		err := q.Delete(utag)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getAllUserTags(wr srv.WrapperRequest) ([]string, error) {
+	tagsMap := make(map[string]int, 0)
+	userTags := NewUserTagBuffer()
+	tags := make([]string, 0)
+
+	q := data.NewConn(wr, "users-tags")
+	err := q.GetMany(&userTags)
+	if err != nil {
+		return tags, err
+	}
+
+	tags = make([]string, len(userTags))
+	for _, utag := range userTags {
+		if _, ok := tagsMap[utag.Tag]; !ok {
+			tagsMap[utag.Tag] = 1
+			tags = append(tags, utag.Tag)
+		}
+	}
+
+	return tags, nil
+}
