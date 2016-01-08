207963aec9ece7cd77e1155149143bd3bddc6fb7
Sergio de Mingo <sdemingo@gmail.com>
2016-01-08 16:00:49 +0100

    clean appengine/srv

--- appengine/srv/server.go
+++ appengine/srv/server.go
@@ -3,8 +3,30 @@ package srv
 import (
 	"net/http"
 
+	"app/users"
+
+	"appengine"
 	"appengine/user"
 )
+
+type WrapperRequest struct {
+	R  *http.Request
+	C  appengine.Context
+	U  *user.User
+	NU users.AppUser
+	//NU           *users.NUser
+
+	JsonResponse bool
+}
+
+func NewWrapperRequest(r *http.Request) WrapperRequest {
+	c := appengine.NewContext(r)
+	return WrapperRequest{r, c, user.Current(c), nil, false}
+}
+
+func (wr WrapperRequest) IsAdminRequest() bool {
+	return user.IsAdmin(wr.C)
+}
 
 func Log(wr WrapperRequest, msg string) {
 	wr.C.Infof("%s", msg)
